31f0fa6cfc14c8b1a8f4b3f8242c4a47afd3faa5
Imad YOUBI IDRISSI <imad.youbi.idrissi@gmail.com>
2024-12-31 00:10:17 +0100

    do: update credentials usage

--- engine/data_pipelines/microservices/google_analytics_pipeline/main_pipeline.py
+++ engine/data_pipelines/microservices/google_analytics_pipeline/main_pipeline.py
@@ -1,30 +1,27 @@
 import os
-
-from google.oauth2.credentials import Credentials
-
+from google.cloud import bigquery
 from engine.data_pipelines.microservices.google_analytics_pipeline import RENAMER_SESSIONS_HITS, QUERY_SESSIONS_HITS
-
 from engine.data_pipelines.common.logging import LOGGER
 from engine.data_pipelines.common.data_ops.extract.bigquery import read_from_bigquery
 from engine.data_pipelines.common.data_ops.transform.stage_columns import column_selection_and_renaming
 
 
 def main():
-
     LOGGER.info("Verify authentication.")
-    # Get the token from the environment variable
-    token = os.getenv("GOOGLE_AUTH_TOKEN")
-    if not token:
-        raise EnvironmentError("GOOGLE_AUTH_TOKEN is not set.")
 
-    # Use the token to create credentials
-    credentials = Credentials(token=token)
+    # Check if GOOGLE_APPLICATION_CREDENTIALS is set
+    credentials_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+    if not credentials_path:
+        raise EnvironmentError("GOOGLE_APPLICATION_CREDENTIALS is not set.")
+
+    # Initialize BigQuery client
+    client = bigquery.Client()
 
     LOGGER.info("Extract data.")
     df = read_from_bigquery(
         query=QUERY_SESSIONS_HITS,
         project_id="dataops-forge",
-        credentials=credentials,
+        credentials=client._credentials,  # Pass client credentials
     )
 
     LOGGER.info("Transform - Staging")
